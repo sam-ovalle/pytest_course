f59314b43d8bc7ae274de0742531f1a1ba822a19
sam-ovalle <samovalle878@gmail.com>
2025-07-28 15:28:28 +0000

    example of a maintainable test flow given->when->then

---
+++ ch02/test_structure.py
@@ -0,0 +1,20 @@
+from cards import Card
+
+# Given/When/Then or Arrange/Act/Assert
+
+
+def test_to_dict():
+    # GIVEN a Card object with known contents
+    c1 = Card("something", "brian", "todo", 123)
+
+    # WHEN we call to_dict() on the object
+    c2 = c1.to_dict()
+
+    # THEN the result will be a dictionary with known content
+    c2_expected = {
+        "summary": "something",
+        "owner": "brian",
+        "state": "todo",
+        "id": 123,
+    }
+    assert c2 == c2_expected
