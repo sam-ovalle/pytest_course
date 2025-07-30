5608ef63ea8bd60c5b48daa0e1ca83628c7cf30a
sam-ovalle <samovalle878@gmail.com>
2025-07-30 18:05:41 +0000

    examples of common built in markers (skip, skipif, xfail)

---
+++ ch06/builtins/test_skipif.py
@@ -0,0 +1,14 @@
+import cards
+import pytest
+from cards import Card
+from packaging.version import parse
+
+
+@pytest.mark.skipif(
+    parse(cards.__version__).major < 2,
+    reason="Card < comparison not supported in 1.x",
+)
+def test_less_than():
+    c1 = Card("a task")
+    c2 = Card("b task")
+    assert c1 < c2
